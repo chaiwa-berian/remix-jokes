c254fee33b57969ce66b9071c0d7e5eb5ea817ac
Chaiwa Berian <chaiwa.berian@gmail.com>
2021-12-17 09:10:32 +0200

    Setup prisma

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,5 @@
 import type { LinksFunction } from "remix";
+import { Link } from "remix";
 import stylesUrl from "../styles/index.css";
 
 export const links: LinksFunction = () => {
@@ -6,5 +7,20 @@ export const links: LinksFunction = () => {
 };
 
 export default function IndexRoute() {
-  return <div>Hello Index Route</div>;
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
